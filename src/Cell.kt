4b224f462ad249e88e17b2caf87bdb83af894610
Hannah <hannahlspencer@gmail.com>
2018-03-04 13:47:18 +0000

    removing cell change method

--- src/Cell.kt
+++ src/Cell.kt
@@ -1,10 +1,7 @@
 class Cell(populated: Boolean = false, col: Int, row: Int) {
     var isPopulated = populated
-    //var neighbourNum = 0;
     private final val columnPosition = col
     private final val rowPosition = row
-
-    //val neighbours: List<Cell>
 
     fun isBorn() {
         isPopulated = true
@@ -23,18 +20,5 @@ class Cell(populated: Boolean = false, col: Int, row: Int) {
         return toPrint
     }
 
-    fun checkNeighbourNum(neighbourNum: Int) {
-        println("Number of neighbours is: $neighbourNum")
-        if(isPopulated) {
-            if(neighbourNum < 2 || neighbourNum > 3) {
-                println("Cell dies")
-                dies()
-            }
-        } else {
-            if(neighbourNum === 3) {
-                println("A cell is born *")
-                isBorn()
-            }
-        }
-    }
+
 }
