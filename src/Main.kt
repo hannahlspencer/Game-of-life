2eb436de5696483ca65fbd80ba428d557473e63d
Hannah <hannahlspencer@gmail.com>
2018-03-04 18:10:22 +0000

    Adding extra pattern and more user choice

--- src/Main.kt
+++ src/Main.kt
@@ -1,16 +1,39 @@
-import java.util.*
+lateinit var game : Grid
+lateinit var pattern1 : Grid
+lateinit var pattern2 : Grid
 
 fun main(array: Array<String>) {
-    var game = Grid(arrayOf(
+    //initialising patterns
+    pattern1 = Grid(arrayOf(
             arrayOf(Cell(false,0,0), Cell(false, 1,0), Cell(false,2,0)),
             arrayOf(Cell(true, 0,1), Cell(true, 1,1), Cell(true, 2,1)),
             arrayOf(Cell(false,0,2), Cell(false, 1,2), Cell(false, 2, 2)))
     )
 
+    pattern2 = Grid(arrayOf(
+            arrayOf(Cell(false,0,0), Cell(false, 1,0), Cell(false,2,0)),
+            arrayOf(Cell(false, 0,1), Cell(true, 1,1), Cell(true, 2,1)),
+            arrayOf(Cell(true,0,2), Cell(true, 1,2), Cell(false, 2, 2)))
+    )
+
+
     var exit = false
+
+    startUp()
+
     game.printBoard()
     while(!exit) {
         exit = nextTurn(game)
+    }
+}
+
+fun startUp() {
+    println("Which pattern would you like to see? Select 1, 2, or 3")
+    var select = readLine()?.toInt()
+    when(select) {
+        1 -> game = pattern1
+        2 -> game = pattern2
+        else -> { println("None of those worked, please start again"); startUp() }
     }
 }
 
