ca69512829f6f4fc8266f5684fc603c547b45e92
Hannah <hannahlspencer@gmail.com>
2018-03-04 17:30:36 +0000

    add toString method; split out cell change methods; add documentation

--- src/Grid.kt
+++ src/Grid.kt
@@ -1,54 +1,80 @@
 import java.util.*
-import java.util.Arrays.copyOf
 
 class Grid(startGrid: Array<Array<Cell>>) {
 
+    //stores current state of the game
     var board = startGrid
-    var tempBoard = Arrays.copyOf(board, board.size)
 
+    /*
+     * Prints current state of the board to the console
+     */
     fun printBoard() {
-
-        for(row in 0..tempBoard.size-1) {
+        for(row in 0..board.size-1) {
             print(" |")
-            for(col in 0..tempBoard[row].size-1) {
-                print(tempBoard[row][col].printCell() + " |")
+            for(col in 0..board[row].size-1) {
+                print(board[row][col].printCell() + " |")
             }
             println();
-
         }
-
     }
 
+    /*
+     * Runs a single turn of the game
+     */
     fun runTurn() {
         cellDecision()
-       // board = tempBoard
         printBoard()
-
     }
 
+    /*
+     * checks each cell and determines its fate
+     * adds cells that need to die or be born to a list
+     * feeds those lists into killOrBirthMethods to kill or birth them
+     */
+
     fun cellDecision() {
+        var toDie = mutableListOf<Cell>()
+        var toBirth = mutableListOf<Cell>()
         for(arr in board) {
             for(cell in arr) {
+                //finds the number of neighbours for each cell
                 var neighbourNum = checkNeighbours(cell);
-                var currentRow = cell.getRowPosition();
-                var currentCol = cell.getColumnPosition()
-                var tempCell = tempBoard[currentRow][currentCol]
 
+                //makes decision based on number of neighbours and whether cell is populated
                 if(cell.isPopulated) {
                     if(neighbourNum < 2 || neighbourNum > 3) {
-                        println("Cell dies")
-                        tempCell.dies()
+                        toDie.add(cell)
                     }
                 } else {
                     if(neighbourNum === 3) {
-                        println("A cell is born *")
-                        tempCell.isBorn()
+                        toBirth.add(cell)
                     }
                 }
             }
+        }
+        //feeds cells to kill or birth into killOrBirth method
+        killOrBirthCells(toDie, toBirth)
+    }
 
+    /*
+     * Takes two lists that are assigned whether they are cells to kill or to birth
+     * Kills or births cells accordingly
+     * @params: List<Cell>, List<Cell>
+     */
+    fun killOrBirthCells(toDie : List<Cell>, toBirth: List<Cell>) {
+        for(cell in toDie) {
+            cell.dies()
+        }
+        for(cell in toBirth) {
+            cell.isBorn()
         }
     }
+
+    /*
+     * Checks each of the cell's neighbours on the board one by one
+     * @param: Cell being checked
+     * @returns: number of neighbours that cell has
+     */
     fun checkNeighbours(cell : Cell) : Int{
         var numNeighbors = 0
         var column = cell.getColumnPosition()
@@ -56,7 +82,7 @@ class Grid(startGrid: Array<Array<Cell>>) {
 
 
         // Up Left
-        if (row - 1 >= 0 && column - 1 >= 0 && board[row-1][column-1].isPopulated)  numNeighbors += 1
+        if (row - 1 >= 0 && column - 1 >= 0 && board[row-1][column-1].isPopulated)numNeighbors += 1
         // Left
         if (row >= 0 && column - 1 >= 0 && board[row][column - 1].isPopulated) numNeighbors += 1
         // Down Left
@@ -72,11 +98,23 @@ class Grid(startGrid: Array<Array<Cell>>) {
         // Up
         if (row - 1 >= 0 && column < board[0].size && board[row - 1][column].isPopulated) numNeighbors += 1
 
-        println("Row =  ${cell.getRowPosition()}")
-        println("Column = ${cell.getColumnPosition()}")
-        println("Neighbours = $numNeighbors")
         return numNeighbors
 
+    }
+
+    /*
+     * overrides toString method to allow for testing
+     * @returns: String representation of the board
+     */
+    override fun toString(): String {
+        var str = ""
+        for(row in 0..board.size-1) {
+            str += (" |")
+            for(col in 0..board[row].size-1) {
+                str += board[row][col].printCell() + " |"
+            }
+        }
+        return str;
     }
 
 }
