e98ac6aacb56cd80a539ad6d06648007e6552f48
Hannah <hannahlspencer@gmail.com>
2018-03-04 17:30:02 +0000

    Tidy & document Cell

--- src/Cell.kt
+++ src/Cell.kt
@@ -1,24 +1,22 @@
 class Cell(populated: Boolean = false, col: Int, row: Int) {
+
     var isPopulated = populated
-    private final val columnPosition = col
-    private final val rowPosition = row
+    private val columnPosition = col
+    private val rowPosition = row
 
-    fun isBorn() {
-        isPopulated = true
-    }
 
-    fun dies() {
-        isPopulated = false
-    }
+    fun isBorn() = {isPopulated = true}
+    fun dies() { isPopulated = false }
 
     fun getColumnPosition() = columnPosition;
     fun getRowPosition() = rowPosition
 
+    /*
+     * return: String of symbol to print based on whether it is populated
+     */
     fun printCell(): String {
         var toPrint = " ";
         if (isPopulated) toPrint = "*" else toPrint = " "
         return toPrint
     }
-
-
 }
